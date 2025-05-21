a57d939bc8b57f3e03fef2479e76ad9a244a707e
javer-123 <fathimajaver01@gmail.com>
2025-05-21 20:36:55 +0530

    Create greatest of 3 nos.py

---
+++ greatest of 3 nos.py
@@ -0,0 +1,5 @@
+a=float(input("Enter number a:")) 
+b=float(input("Enter number b:")) 
+c=float(input("Enter number c:")) 
+greatest_number=max(a,b,c) 
+print("The greatest nunber is: " ,greatest_number)
